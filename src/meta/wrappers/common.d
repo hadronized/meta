8ec801ad7fc00554a4625c49c43418621a4b63dc
Dimitri <sabadie.dimitri@gmail.com>
2012-06-02 16:35:56 +0200

    Added a common file for wrappers.

---
+++ src/meta/wrappers/common.d
@@ -0,0 +1,15 @@
+module meta.wrappers.common;
+
+/* imports */
+private {
+}
+public {
+	import meta.utils.runtime_error;
+}
+
+/* runtime error */
+class wrapper_not_loaded : runtime_error {
+    this(string wrapper, string reason) {
+        super("failed to load the wrapper \'" ~ wrapper ~ "\'; reason: " ~ reason);
+    }
+}
