d5584173236670fd691de7cc1cd052febaa09339
Dimitri <sabadie.dimitri@gmail.com>
2012-06-29 21:06:18 +0200

    Merge player into sample class.

--- src/meta/wrappers/bass/sample.d
+++ src/meta/wrappers/bass/sample.d
@@ -16,8 +16,7 @@ class sample_loading_error : runtime_error {
     }
 }
 
-/* exception when there's some trouble with sample's channel */
-class sample_channel_error : runtime_error {
+class sample_error : runtime_error {
     this(string reason) {
         super("sample channel error; reason: " ~ reason);
     }
@@ -40,8 +39,32 @@ class sample {
     HCHANNEL channel() const @property {
         auto chan = BASS_SampleGetChannel(_sHnd, false); 
         if (!chan)
-            throw new sample_channel_error("unable to retreive the channel of a sample");
+            throw new sample_error("unable to retreive the channel of a sample");
         return chan;
     }
+
+
+    void play() {
+        auto played = BASS_ChannelPlay(channel, true);
+        if (!played)
+            throw new sample_error("unable to play a sample");
+    }
+
+    double cursor() const @property {
+        auto chan = channel;
+
+        /* first, get the cursor position */
+        auto bcurs = BASS_ChannelGetPosition(chan, BASS_POS_BYTE);
+        if (bcurs == -1)
+            throw new sample_error("unable to get cursor position");
+
+        /* then, translate it into seconds */
+        auto s = BASS_ChannelBytes2Seconds(chan, bcurs);
+        if (s < 0)
+            throw new sample_error("unable to translate cursor position");
+
+        return s;
+    }
+
 }
 
