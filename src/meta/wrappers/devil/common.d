89ad17c4752f28667bac50f0d6edd0013a9dd638
Dimitri <sabadie.dimitri@gmail.com>
2012-07-11 16:10:53 +0200

    Added devil wrapper.

---
+++ src/meta/wrappers/devil/common.d
@@ -0,0 +1,26 @@
+module meta.wrappers.devil.common;
+
+private {
+    import meta.wrappers.common;
+}
+public {
+    import derelict.devil.il;
+    import meta.utils.logger;
+    import meta.utils.runtime_error;
+}
+
+
+enum WRAPPER_NAME = "devil";
+
+/* static ctor */
+static this() {
+    logger.inst().deb("Initializing " ~ WRAPPER_NAME ~ " module");
+
+    /* devil initialization */
+    DerelictIL.load();
+    if (!DerelictIL.isLoaded())
+        throw new wrapper_not_loaded(WRAPPER_NAME, "unknown");
+
+    logger.inst().deb("Successfully initialized " ~ WRAPPER_NAME ~ " module");
+}
+
