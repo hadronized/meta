a63f20aa2970fd5ba3a7aeb22a64eb6ec1949c03
Dimitri <sabadie.dimitri@gmail.com>
2012-07-11 16:13:16 +0200

    Untabified.

--- src/meta/wrappers/devil/image.d
+++ src/meta/wrappers/devil/image.d
@@ -2,65 +2,65 @@ module meta.wrappers.devil.image;
 
 /* imports */
 private {
-	import std.string : toStringz;
-	import meta.wrappers.devil.common;
-	import meta.wrappers.opengl.texture : texture_format;
+    import std.string : toStringz;
+    import meta.wrappers.devil.common;
+    import meta.wrappers.opengl.texture : texture_format;
 }
 public {
 }
 
 
 class image {
-	private {
-		ILuint _;
-		ILuint _w;
-		ILuint _h;
-		ILint _format;
-		ILuint _bpp;
-		ILubyte *_data;
-	}
+    private {
+        ILuint _;
+        ILuint _w;
+        ILuint _h;
+        ILint _format;
+        ILuint _bpp;
+        ILubyte *_data;
+    }
 
-	@property {
-		uint width() const {
-			return _w;
-		}
+    @property {
+        uint width() const {
+            return _w;
+        }
 
-		uint height() const {
-			return _;	
-		}
+        uint height() const {
+            return _;    
+        }
 
-		texture_format format() const {
-			return _format;	
-		}
+        texture_format format() const {
+            return _format;    
+        }
 
-		uint bpp() const {
-			return _bpp;
-		}
+        uint bpp() const {
+            return _bpp;
+        }
 
-		const(ubyte) * data() const {
-			return _data;	
-		}
-	}
+        const(ubyte) * data() const {
+            return _data;    
+        }
+    }
 
-	this() {
-		ilGenImages(1, &_);
-	}
+    this() {
+        ilGenImages(1, &_);
+    }
 
-	~this() {
-		ilDeleteImages(1, &_);
-	}
+    ~this() {
+        ilDeleteImages(1, &_);
+    }
 
-	void bind() {
-		ilBindImage(_);
-	}
+    void bind() {
+        ilBindImage(_);
+    }
 
-	void from_disk(string file) {
-		bind();
-		ilLoadImage(toStringz(file));
-		_w = ilGetInteger(IL_IMAGE_WIDTH);
-		_h = ilGetInteger(IL_IMAGE_HEIGHT);
-		_format = ilGetInteger(IL_IMAGE_FORMAT);
-		_bpp = ilGetInteger(IL_IMAGE_BYTES_PER_PIXEL);
-		_data = ilGetData();
-	}
+    void from_disk(string file) {
+        bind();
+        ilLoadImage(toStringz(file));
+        _w = ilGetInteger(IL_IMAGE_WIDTH);
+        _h = ilGetInteger(IL_IMAGE_HEIGHT);
+        _format = ilGetInteger(IL_IMAGE_FORMAT);
+        _bpp = ilGetInteger(IL_IMAGE_BYTES_PER_PIXEL);
+        _data = ilGetData();
+    }
 }
